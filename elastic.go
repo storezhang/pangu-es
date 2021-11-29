d977fd895cfd5196e925434ec93758e12d322b58
ws.zhang <willzhang@hanyuwangxue.com>
2021-11-29 16:09:19 +0800

    集成ElasticSearch

--- elastic.go
+++ elastic.go
@@ -1,9 +1,44 @@
 package elasticsearch
 
 import (
+	"net"
+	"net/http"
+
+	"github.com/olivere/elastic/v7"
 	"github.com/pangum/pangu"
 )
 
-func newElasticClient(config *pangu.Config) {
+func newElasticClient(config *pangu.Config) (client *Client, err error) {
+	_panguConfig := new(panguConfig)
+	if err = config.Load(_panguConfig); nil != err {
+		return
+	}
 
+	_conf := _panguConfig.ElasticSearch
+	// 配置http client
+	httpClient := new(http.Client)
+	httpClient.Transport = &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   _conf.Http.Dial.TimeOut,
+			KeepAlive: _conf.Http.Dial.KeepAlive,
+		}).DialContext,
+		MaxIdleConns:          _conf.Http.Connections.Idle.Max,
+		MaxIdleConnsPerHost:   _conf.Http.Connections.Idle.PerHost,
+		MaxConnsPerHost:       _conf.Http.Connections.MaxPerHost,
+		IdleConnTimeout:       _conf.Http.Connections.Idle.TimeOut,
+		TLSHandshakeTimeout:   _conf.Http.Connections.TLS.TimeOut,
+		ExpectContinueTimeout: _conf.Http.Connections.Wait,
+		ForceAttemptHTTP2:     _conf.Http.Connections.Http2,
+	}
+
+	if client.Client, err = elastic.NewClient(
+		elastic.SetHttpClient(httpClient),
+		elastic.SetBasicAuth(_conf.Username, _conf.Password),
+		elastic.SetURL(_conf.Address),
+		elastic.SetSniff(_conf.Sniff),
+	); nil != err {
+		return
+	}
+
+	return
 }
--- elastic_http.go
+++ elastic_http.go
@@ -1,14 +1,37 @@
 package elasticsearch
 
+import (
+	"time"
+)
+
 type elasticHttp struct {
-	TimeOut               int64 `default:"10" yaml:"timeOut"`
-	KeepAlive             int64 `default:"30" yaml:"keepAlive"`
-	MaxIdleConns          int64 `default:"300" yaml:"maxIdleConns"`
-	MaxIdleConnsPerHost   int64 `default:"150" yaml:"maxIdleConnsPerHost"`
-	MaxConnsPerHost       int64 `default:"300" yaml:"maxConnsPerHost"`
-	IdleConnTimeout       int64 `default:"60" yaml:"idleConnTimeout"`
-	TLSHandshakeTimeout   int64 `default:"60" yaml:"tlsHandshakeTimeout"`
-	ExpectContinueTimeout int64 `default:"1" yaml:"expectContinueTimeout"`
-	// 强制尝试Http2
-	ForceAttemptHTTP2 bool `default:"true" yaml:"forceAttemptHTTP2"`
+	Dial struct {
+		TimeOut   time.Duration `default:"10s" yaml:"timeout"`
+		KeepAlive time.Duration `default:"30s" yaml:"keepalive"`
+	} `yaml:"dial"`
+
+	// Connections 连接配置
+	Connections struct {
+		// 单个主机最大连接数
+		MaxPerHost int `default:"300" yaml:"maxPerHost"`
+		// ExpectContinueTimeout
+		Wait time.Duration `default:"1s" yaml:"wait"`
+		// 强制尝试Http2
+		Http2 bool `default:"true" yaml:"http2"`
+		// 空闲连接配置
+		Idle struct {
+			// 最大空闲连接数
+			Max int `default:"300" yaml:"max"`
+			// 单个主机最大空闲连接数
+			PerHost int `default:"150" yaml:"perHost"`
+			// 连接超时时间
+			TimeOut time.Duration `default:"60s" yaml:"timeout"`
+		} `yaml:"idle"`
+
+		// TLS 安全传输配置
+		TLS struct {
+			// 握手超时时间
+			TimeOut time.Duration `default:"60s" yaml:"timeout"`
+		} `yaml:"tls"`
+	} `yaml:"connections"`
 }
