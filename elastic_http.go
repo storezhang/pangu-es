822d646c0c008503fc9f4d6c427d465767b7d8c8
usmile-ok <62837413+usmile-ok@users.noreply.github.com>
2021-11-30 09:44:55 +0800

    集成ElasticSearch

---
+++ elastic_http.go
@@ -0,0 +1,37 @@
+package elasticsearch
+
+import (
+	"time"
+)
+
+type elasticHttp struct {
+	Dial struct {
+		TimeOut   time.Duration `default:"10s" yaml:"timeout"`
+		KeepAlive time.Duration `default:"30s" yaml:"keepalive"`
+	} `yaml:"dial"`
+
+	// Connections 连接配置
+	Connections struct {
+		// 单个主机最大连接数
+		MaxPerHost int `default:"300" yaml:"maxPerHost"`
+		// ExpectContinueTimeout
+		Wait time.Duration `default:"1s" yaml:"wait"`
+		// 强制尝试Http2
+		Http2 bool `default:"true" yaml:"http2"`
+		// 空闲连接配置
+		Idle struct {
+			// 最大空闲连接数
+			Max int `default:"300" yaml:"max"`
+			// 单个主机最大空闲连接数
+			PerHost int `default:"150" yaml:"perHost"`
+			// 连接超时时间
+			TimeOut time.Duration `default:"60s" yaml:"timeout"`
+		} `yaml:"idle"`
+
+		// TLS 安全传输配置
+		TLS struct {
+			// 握手超时时间
+			TimeOut time.Duration `default:"60s" yaml:"timeout"`
+		} `yaml:"tls"`
+	} `yaml:"connections"`
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/usmile-ok/elasticsearch
+
+go 1.17
+
+require (
+	github.com/pangum/pangu v0.0.9
+	github.com/olivere/elastic/v7 v7.0.19
+)
