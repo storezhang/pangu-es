350f14f2f5d76cfd2b72f997ed1b2abf3b0c6ec1
usmile-ok <62837413+usmile-ok@users.noreply.github.com>
2021-12-01 16:47:36 +0800

    集成ElasticSearch

--- client.go
+++ client.go
@@ -7,3 +7,25 @@ import (
 type Client struct {
 	*elastic.Client
 }
+
+func (c *Client) GetByDocId(index string, docId string, result interface{}) (exists bool, err error) {
+	var rsp *elastic.GetResult
+	rsp, err = c.Get().Index(index).Id(docId).Do(context.Background())
+	if nil != err {
+		if elasticErr, ok := err.(*elastic.Error); ok {
+			if http.StatusNotFound == elasticErr.Status {
+				err = nil
+			}
+		}
+	}
+	if nil == rsp || !rsp.Found || nil == rsp.Source {
+		return
+	}
+	if err = json.Unmarshal(rsp.Source, result); nil != err {
+		return
+	}
+
+	exists = true
+
+	return
+}
