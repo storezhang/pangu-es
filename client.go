53a60b234fea17881b5f6c70469e6b89e8d1812c
ws.zhang <willzhang@hanyuwangxue.com>
2021-12-01 17:11:42 +0800

    增加api

--- client.go
+++ client.go
@@ -1,42 +1,9 @@
 package elasticsearch
 
 import (
-	`context`
-	`encoding/json`
-	`net/http`
-
 	"github.com/olivere/elastic/v7"
 )
 
 type Client struct {
 	*elastic.Client
 }
-
-func (c *Client) Save(index string, docId string, bean interface{}) (err error) {
-	_, err = c.Index().Index(index).Id(docId).BodyJson(bean).Refresh("true").Do(context.Background())
-
-	return
-}
-
-func (c *Client) GetByDocId(index string, docId string, result interface{}) (exists bool, err error) {
-	var rsp *elastic.GetResult
-	rsp, err = c.Get().Index(index).Id(docId).Do(context.Background())
-	if nil != err {
-		if elasticErr, ok := err.(*elastic.Error); ok {
-			if http.StatusNotFound == elasticErr.Status {
-				err = nil
-			}
-		}
-	}
-	if nil == rsp || !rsp.Found || nil == rsp.Source {
-		return
-	}
-	if err = json.Unmarshal(rsp.Source, result); nil != err {
-		return
-	}
-
-	exists = true
-
-	return
-}
-
